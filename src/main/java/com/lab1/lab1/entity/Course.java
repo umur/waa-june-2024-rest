6749bbdc35456674509829c21c85b032ceebc409
hariclerry <clerryha@gmail.com>
2024-06-25 14:28:05 -0500

    add course and student entity

---
+++ src/main/java/com/lab1/lab1/entity/Course.java
@@ -0,0 +1,12 @@
+package com.lab1.lab1.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Course {
+    private int id;
+    private String name;
+    private String code;
+}
---
+++ src/main/java/com/lab1/lab1/entity/Student.java
@@ -0,0 +1,19 @@
+package com.lab1.lab1.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
+@Getter
+public class Student {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String major;
+    private int gpa;
+    private List<Course> coursesTaken = new ArrayList<>();
+}
