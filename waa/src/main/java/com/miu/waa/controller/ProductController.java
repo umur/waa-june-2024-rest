9b401c4323441c5b4420b8756bb4abeb50064c2d
Darkhan Zhursin <daratechn@gmail.com>
2024-06-25 13:36:26 -0500

    inital commit

---
+++ waa/src/main/java/com/miu/waa/controller/ProductController.java
@@ -0,0 +1,48 @@
+package com.miu.waa.controller;
+
+import com.miu.waa.entity.Product;
+import com.miu.waa.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping
+    public List<Product> getProducts() {
+        return productService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product findById(@PathVariable Long id) {
+        return productService.findById(id);
+    }
+
+    @PostMapping
+    public Product create(@RequestBody Product product) {
+        productService.save(product);
+        return product;
+    }
+
+    @PutMapping
+    public Product update(@RequestBody Product product) {
+        productService.update(product);
+        return product;
+    }
+
+    @DeleteMapping
+    public void delete(@RequestBody Product product) {
+        productService.delete(product.getId());
+    }
+
+    @GetMapping("/filter")
+    public List<Product> filterByColor(@RequestParam String color) {
+        return null;
+    }
+}
---
+++ waa/src/main/java/com/miu/waa/entity/Product.java
@@ -0,0 +1,11 @@
+package com.miu.waa.entity;
+
+import lombok.Data;
+
+@Data
+public class Product {
+
+    private Long id;
+    private String name;
+    private int price;
+}
